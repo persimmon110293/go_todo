95ca220d07b7aaf1a5394256941356f03cc824d7
Shohei Kakimoto <shohei.11293@gmail.com>
2024-04-01 22:50:07 +0900

    feature/#8 make Todo common

--- go/src/usecase/todo_usecase.go
+++ go/src/usecase/todo_usecase.go
@@ -2,14 +2,15 @@ package usecase
 
 import (
 	"encoding/json"
+	entity "main/domain/entity"
 	"main/repository"
 )
 
 type ITodoUsecase interface {
-	GetAllTodo() ([]repository.Todo, error)
-	GetTodoById(string) (*repository.Todo, error)
-	CreateTodo([]byte) (*repository.Todo, error)
-	UpdateTodoById(string, []byte) (*repository.Todo, error)
+	GetAllTodo() ([]entity.Todo, error)
+	GetTodoById(string) (*entity.Todo, error)
+	CreateTodo([]byte) (*entity.Todo, error)
+	UpdateTodoById(string, []byte) (*entity.Todo, error)
 	DeleteTodoById(string) error
 }
 
@@ -21,7 +22,7 @@ func NewTodoUsecase(repository repository.ITodoRepository) ITodoUsecase {
 	return &TodoUsecase{repository}
 }
 
-func (u *TodoUsecase) GetAllTodo() ([]repository.Todo, error) {
+func (u *TodoUsecase) GetAllTodo() ([]entity.Todo, error) {
 	todos, err := u.repository.GetAllTodo()
 	if err != nil {
 		return nil, err
@@ -30,7 +31,7 @@ func (u *TodoUsecase) GetAllTodo() ([]repository.Todo, error) {
 	return todos, nil
 }
 
-func (u *TodoUsecase) GetTodoById(id string) (*repository.Todo, error) {
+func (u *TodoUsecase) GetTodoById(id string) (*entity.Todo, error) {
 	todo, err := u.repository.GetTodoById(id)
 	if err != nil {
 		return nil, err
@@ -39,7 +40,7 @@ func (u *TodoUsecase) GetTodoById(id string) (*repository.Todo, error) {
 	return todo, nil
 }
 
-func (u *TodoUsecase) CreateTodo(body []byte) (*repository.Todo, error) {
+func (u *TodoUsecase) CreateTodo(body []byte) (*entity.Todo, error) {
 	var message map[string]string
 
 	// JSONをパース
@@ -50,7 +51,7 @@ func (u *TodoUsecase) CreateTodo(body []byte) (*repository.Todo, error) {
 	return u.repository.CreateTodo(message)
 }
 
-func (u *TodoUsecase) UpdateTodoById(id string, body []byte) (*repository.Todo, error) {
+func (u *TodoUsecase) UpdateTodoById(id string, body []byte) (*entity.Todo, error) {
 	var message map[string]string
 
 	// JSONをパース
