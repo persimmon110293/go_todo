a3121de5b2f22fb4c9575535468d090bd92ab52b
Shohei Kakimoto <shohei.11293@gmail.com>
2024-04-21 18:04:26 +0900

    feature/#13 added test code

--- go/src/handler/todo_handler_test.go
+++ go/src/handler/todo_handler_test.go
@@ -1,7 +1,26 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetAllTodo(t *testing.T) {}
+// FIXME: テストコード書く
+// DNSでエラーが発生する
+func TestGetAllTodo(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		router := gin.Default()
+		w := httptest.NewRecorder() // httptestの使い方調べてみる
+
+		req, _ := http.NewRequest("GET", "/todo", nil)
+		router.GET("/todo", GetAllTodo)
+		router.ServeHTTP(w, req)
+
+		assert.NotNil(t, w.Body)
+		assert.Equal(t, w.Code, 200)
+	})
+}
